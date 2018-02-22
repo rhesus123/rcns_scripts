2b41677cb638e815314cb21a8c65d0b048a2b259
TravisCG <buldozer88@hotmail.com>
2018-02-22 10:25:11 +0100

    Another fine work

--- gtrdownload.py
+++ gtrdownload.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+"""
+   Script to download genes and conditions from NCBI's GTR website
+   The input parameter is the ID of the condition. For example for
+   medulloblastoma one can use the following command line:
+   python3 gtrdownload.py C0025149
+   The output is the test, the affected gene, condition and the offerer of the test
+"""
+
 import sys
 import urllib.request
+import re
 from xml.dom import minidom
 
 def download(url):
@@ -10,6 +19,9 @@ def download(url):
     return(xml)
 
 term = sys.argv[1]
+pattern = re.compile("<[^>]+>")
+newline = re.compile("<br/>")
+whitesp = re.compile("\s+")
 
 tests = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gtr&term=" + term + "&retstart=0&retmax=300" # later we will made it more appropriate
 xml = download(tests)
@@ -22,11 +34,14 @@ for i in ids:
     testname = xml2.getElementsByTagName("TestName")[0].childNodes[0].data
     offerer  = xml2.getElementsByTagName("Offerer")[0].childNodes[0].data
     analytes = xml2.getElementsByTagName("Analyte")
-    for analyte in analytes:
-        genename = analyte.getElementsByTagName("Name")[0].childNodes[0].data
-        location = analyte.getElementsByTagName("Location")[0].childNodes[0].data
-        #print(testname, offerer, genename, location, sep = "\t")
     cond     = "https://www.ncbi.nlm.nih.gov/gtr/tests/" + gtestid + "/methodology/"
     xml2     = download(cond)
-    table    = xml2.getElementsByTagName("table")[0]
-    print(table)
+    table    = xml2.getElementsByTagName("tbody")[0]
+    rows     = table.getElementsByTagName("tr")
+    for row in rows:
+        columns = row.getElementsByTagName("td")
+        first   = pattern.sub(" ", columns[0].toxml())
+        last    = newline.sub("|", columns[-1].toxml())
+        last    = pattern.sub(" ", last)
+        last    = whitesp.sub(" ", last)
+        print(testname, first, last, offerer, sep = "\t")
